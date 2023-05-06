57bc6a6e9b9951d8c3b89f369ae5f78b66e86e7d
Juwoong Bae <to@juwoong.me>
2023-05-06 23:03:16 +0900

    feat: dockerize lec03

--- lec03/with_session.py
+++ lec03/with_session.py
@@ -35,4 +35,4 @@ def index():
     return render_template('index.html', is_login=is_login, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8081, host="0.0.0.0")
